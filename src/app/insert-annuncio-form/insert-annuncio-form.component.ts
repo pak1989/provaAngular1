f827ed143d23af7fcab4baa4b8ceab0953c804eb
pak1989 <paolok89@gmail.com>
2019-03-14 16:58:42 +0100

    studying form builder

--- src/app/insert-annuncio-form/insert-annuncio-form.component.ts
+++ src/app/insert-annuncio-form/insert-annuncio-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Annuncio } from '../models/annuncio';
-import { FormGroup, FormControl } from '@angular/forms';
+import {  FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-insert-annuncio-form',
@@ -10,25 +10,26 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class InsertAnnuncioFormComponent implements OnInit {
 
-  constructor() { }
+  categoryForm: FormGroup;
+  category : any= [
+    { "id": 1, "nome": "Elettronica" },
+    { "id": 2, "nome": "Giardinaggio"},
+    { "id": 3, "nome": "Informatica"}
+  ]
+
+  constructor( private fb: FormBuilder ) { 
+    const formControls = this.category.map(control => new FormControl(false));
+
+    const selectAllControl = new FormControl(false);
+
+    this.categoryForm = this.fb.group({
+      category: new FormArray(formControls),
+      selectAll: selectAllControl 
+    });
+   }
 
   ngOnInit() {
   }
-
-  category : any= [
-    {
-      "id": 1,
-      "nome": "Elettronica"
-    },
-    {
-      "id": 2,
-      "nome": "Giardinaggio"
-    },
-    {
-      "id": 3,
-      "nome": "Informatica"
-    }
-  ]
 
 /*   sampleForm = new FormGroup({
     firstName: new FormControl(''),
@@ -37,18 +38,23 @@ export class InsertAnnuncioFormComponent implements OnInit {
     category: new FormControl(this.category)
   }); */
 
-  model = new Annuncio(1, true, "testo", 5, this.category, "ut");
+  model = new Annuncio(1, true, "testo", 5, [], "ut");
 
   submitted = false;
 
   onSubmit() { this.submitted = true; }
 
   save() {
-    console.log(this.model)
+      // Filter out the unselected ids
+      const selectedPreferences = this.categoryForm.value.category
+        .map((checked, index) => checked ? this.category[index].id : null)
+        .filter(value => value !== null);
+        console.log(this.categoryForm.value)
   }
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
 
+  
 
 }
